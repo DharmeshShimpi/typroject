62845f50cbca20b77655294ca84e922e8faa2607
Dharmesh Shimpi <dharmeshshimpi7@gmail.com>
2026-09-11 10:08:11 +0530

    feat(auth): user registration using supabase at /api/auth/register

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 
 1. tanuja built register, login and landing pages. also wrote landing.css and style.css in frontend folder. push date: 9/8/2026
 
-2. 
+2. //comments by Dharmesh
+// in dev Access-Control-Allow-Origin: * is by default which means any frontend can access our backend. But during prod. we will restrict it to https://our-project-name.vercel.app
+// code for it will be like this:
+//app.use(cors({origin: 'https://our-project-name.vercel.app'}));
+//this is basic browser level security. we should not only rely on this because anyone can use cURL or Postman to bypass CORS.
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { supabase } from './supabaseClient.js' ;
 
 dotenv.config();
 
@@ -8,11 +9,67 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({message: "Backend running"})
+    res.json({message: "Backend running"});
 })
+
+app.post('/api/auth/register', async(req, res) => {
+    try{
+    const {name, email, password, role, rollno} = req.body;
+
+    // validation start
+    if(!name || !email || !role || !password) {
+        return res.status(400).json({
+            success: false,
+            message: 'All field (name, email, roll and password) are required.'
+        });
+    }
+
+    if(role === 'student' && !rollno) {
+        return res.status(400).json({
+            success: false,
+            message: 'Roll number is required for students.'
+        });
+    }
+    // validation end
+
+    // supabase create user
+    const {data, error} = await supabase.auth.signUp({
+        email: email,
+        password: password,
+        options: {
+            data: {
+                name: name,
+                role: role,
+                rollno: rollno || null
+            }
+        }
+    });
+
+    //supabase error on create user if any
+    if(error) {
+        return res.status(400).json({
+            message: error.message
+        });
+    }
+
+    //success message from supabase if registration is successful
+    return res.status(201).json({
+        success: true,
+        message: 'Registration successful!',
+        user: data.user
+    });
+
+    } catch (err) {
+        console.error('Server error', err);
+        return res.status(500).json({
+            success:false,
+            message: 'Internal server error'
+        });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
